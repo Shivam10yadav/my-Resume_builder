5b5200bb9013d47b7bdf7366a1975964bcda4a79
Shivam Yadav <shivam10yadav05@gmail.com>
2025-11-24 17:04:44 +0530

    Update Api URL and CORS for deployment

--- server/server.js
+++ server/server.js
@@ -14,7 +14,10 @@ const PORT=process.env.PORT ||3000;
 await connectDb() 
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin:"https://starresume.vercel.app/",
+  credentials:true
+}))
 
 app.get('/',(req,res)=>{
   res.send("server is live...")
